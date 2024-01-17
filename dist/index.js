1fe6d1eeedf92b2a64939577a553b4a5543d2c68
vector-10 <duzieblaise10@gmail.com>
2024-01-17 17:06:24 +0100

    chore: setup node.js with typescript

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-"use strict";
-console.log(123);
-const addNumber = (a, b) => {
-    return a + b;
-};
-addNumber(1, 2);
--- server/index.ts
+++
@@ -1,8 +0,0 @@
-console.log(123);
-
-
-const addNumber = (a: number, b: number) => {
-    return a + b
-}
-
-addNumber(1, 2)
